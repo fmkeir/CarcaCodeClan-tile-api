23e9ebbaafe576a835338eb7fa3e2340660f204a
FKeir <fmkeir@gmail.com>
2020-03-19 15:18:05 +0000

    Add configuration for heroku

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -1,12 +1,21 @@
 require('pg')
+require('uri')
 
 class SqlRunner
   def self.run(sql,values = [])
+    if ENV["DATABASE_URL"]
+      uri = URI.parse(ENV["DATABASE_URL"])
+      database_config = {
+        host: uri.host,
+        dbname: uri.path[1..-1],
+        user: uri.user,
+        password: uri.password
+      }
+    else
+      database_config = {host: "localhost", dbname: "tiles"}
+    end
     begin
-      db = PG.connect ({
-        dbname: 'tiles',
-        host: 'localhost'
-        })
+      db = PG.connect(database_config)
       db.prepare("query", sql)
       result = db.exec_prepared("query", values)
     ensure
