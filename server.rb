60283f98bea28806b5d8166da1cca37ae71fb2c2
FKeir <fmkeir@gmail.com>
2020-03-19 15:08:33 +0000

    Add image urls. Refactor server.rb to use a controller

--- server.rb
+++ server.rb
@@ -1,22 +1,13 @@
 require 'sinatra'
 require 'sinatra/contrib/all'
-require_relative './schemas/TileSchema'
-require_relative './models/tile'
+require_relative './controllers/tile_controller_v1.rb'
 also_reload 'models/*'
 
 before do
-   pass if request.path_info.split('/')[1] != "api"
-   content_type :json
- end
+  pass if request.path_info.split('/')[1] != "api"
+  content_type :json
+end
 
 get '/' do
   erb :index
-end
-
-get '/api/tiles' do
-  TileSchema.dump_all Tile.all
-end
-
-get '/api/tiles/:id' do
-  TileSchema.dump Tile.find params[:id]
 end
