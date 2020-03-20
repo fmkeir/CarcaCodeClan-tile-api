1feede673c3752f6c68f774b8233b33afef76f05
FKeir <fmkeir@gmail.com>
2020-03-20 11:49:54 +0000

    Allow cross origin requests

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
-source('https://rubygems.org') do
-  gem('sinatra')
-  gem('sinatra-contrib')
-  gem('pg')
+source 'https://rubygems.org' do
+  gem 'sinatra'
+  gem 'sinatra-contrib'
+  gem 'sinatra-cross_origin'
+  gem 'pg'
 end
--- server.rb
+++ server.rb
@@ -1,10 +1,17 @@
 require 'sinatra'
 require 'sinatra/contrib/all'
+require 'sinatra/cross_origin'
 require_relative './controllers/tile_controller_v1.rb'
 also_reload 'models/*'
 
+configure do
+  enable :cross_origin
+end
+
 before do
   pass if request.path_info.split('/')[1] != "api"
+
+  response.headers['Access-Control-Allow-Origin'] = '*'
   content_type :json
 end
 
